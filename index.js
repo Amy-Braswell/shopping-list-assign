9e29c5327e5242b6697d61f9fdfc1157edcad14d
Amy Braswell <braswellamy@mac.com>
2019-09-30 21:12:18 -0500

    Add Item Works / StrikeThrough Works / Delete Item Works / NEED TO ADD EVENT DELEGATION TO ADDED ITEM

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
  
-//To let user enter their item in the input box and make 'Add Item' button work
+//Takes user input and creates new list item on shopping list
 $(function() {
-    $('#js-shopping-list-form').submit(event => {
-      // this stops the default form submission behavior
-      event.preventDefault();
-      const userTextElement = $(event.currentTarget).find('#shopping-list-entry');
-      $(".js-display-user-text").text(`user text is:  ${userTextElement.val()}`);
-      userTextElement.val("");
+  $('#js-shopping-list-form').submit(event => {
+    // this stops the default form submission behavior
+    event.preventDefault();
+    // stores user input in a variable
+    const userTextElement = $(event.currentTarget).find('#shopping-list-entry');
+
+//places variable storing user input in a string literal that is prepended to the ul
+  $("ul.shopping-list").prepend(`<li>
+      <span class="shopping-item">${userTextElement.val()}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle">
+          <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete">
+          <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>`)
+    //empties input box after user input is added to shopping list
+    userTextElement.val("");
     }); 
-    
-  });
-  
+});
 
-   //To strikethough the list item text when user clicks the 'Check' button 
-   //???? How do I target JUST this grocery item ???? Can't figure out how to make
-   // .closest work here...
-   $('.shopping-item-toggle').on('click', strikeListItem);
-   function strikeListItem() {
-     
-     $( this ).toggleClass('shopping-item__checked'); // strikes button
+//To strikethough the list item text when user clicks the 'Check' button 
+$('.shopping-item-toggle').on('click', strikeListItem);
+  function strikeListItem() {
+  //creates variable to find the <li> that contains the delete button which was clicked.
+  const closestParentLI = $( event.target ).closest( "li" );
+  //finds the span with the class "shopping-item" within the SAME <li> and toggles the class
+  closestParentLI.find("span.shopping-item").toggleClass('shopping-item__checked');
+  };
 
-     $('span.shopping-item').toggleClass('shopping-item__checked');  // strikes all li's
-   };
-
-
-
-  //To remove the list item when user clicks the 'Delete' button
-  //????? Once a user clicks a 'Delete' button then ANY other button clicked will remove 
-  //the list item, whether the button is a 'Check' or 'Delete' button ????
-  $('.shopping-item-delete').on('click', removeListItem);
-    function removeListItem() {
-         // function targets that list item
-         $( event.target ).closest( "li" );
-         // removes that li 
-         $('li').click(function(event) {
-             this.remove();
-        });
-    };
+//To remove the list item when user clicks the 'Delete' button
+$('.shopping-item-delete').on('click', removeListItem);
+  function removeListItem() {
+  // function targets that list item and ONLY that list item
+  $( event.target ).closest( "li" ).remove();
+  };
 
 
 
